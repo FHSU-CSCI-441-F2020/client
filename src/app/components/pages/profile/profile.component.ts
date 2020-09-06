2e8a141cf058a8aea7221495e01c28a4a6709aa7
Unknown <Deadstylebp@gmail.com>
2020-09-06 13:35:57 -0500

    Corrected issues with schema, added completed profile

--- src/app/components/pages/profile/profile.component.ts
+++ src/app/components/pages/profile/profile.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { UserProfile } from '../../../models/userProfile';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  public profile: UserProfile;
+  public user: User;
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getUser().subscribe((user) => {
+      this.user = { ...user };
+    });
+    // this.user = this.userService.getUser();
+    this.userService.getUserProfile().subscribe((profile) => {
+      this.profile = { ...profile };
+      console.log(profile.address2);
+    });
   }
-
 }
